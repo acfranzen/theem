ff0230f52c61ba9a9137f0701d480de850f1afbc
Alex Franzen <acfranzen@gmail.com>
2025-02-28 13:46:30 +0100

    fix build

--- app/picker/page.tsx
+++ app/picker/page.tsx
@@ -1,5 +1,4 @@
 import ThemeCreator from '@/components/picker/theme-creator';
-import { ContentLayout } from '@/components/sidebar/shadcn/content-layout';
 
 export default function Home() {
   return (
